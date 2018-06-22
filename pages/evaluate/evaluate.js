ca532942ab69ed8a36d4993ee7a9149e0b058fc8
weipo0105 <weipo0105@126.com>
2018-06-22 15:49:51 +0800

    优化了测评三步骤样式，报告页面框架搭建完成

--- pages/evaluate/evaluate.js
+++ pages/evaluate/evaluate.js
@@ -5,7 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    showView:false
+    showView1: true,
+    showView2: false,
+    showView3: false
   },
 
   /**
@@ -14,53 +16,34 @@ Page({
   onLoad: function (options) {
   
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  toStep2:function(){
+    
+    this.setData({
+      showView1: false,
+      showView2: true,
+      showView3: false
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  toStep1: function () {
+    
+    this.setData({
+      showView1: true,
+      showView2: false,
+      showView3: false
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  toStep3: function () {
+    
+    this.setData({
+      showView1: false,
+      showView2: false,
+      showView3: true
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  toReport:function(){
+    wx.navigateTo({
+      url:'/pages/report/report'
+    });
   }
+
 })
