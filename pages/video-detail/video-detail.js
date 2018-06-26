4181d444072b6755de872eac952904db7db47f02
weipo0105 <weipo0105@126.com>
2018-06-26 15:11:53 +0800

    讲师介绍-展开-收起-完成

--- pages/video-detail/video-detail.js
+++ pages/video-detail/video-detail.js
@@ -5,7 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    isShow:true,
+    arrow:'up',
+    showText:'收起'
   },
 
   /**
@@ -15,52 +17,21 @@ Page({
   
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  onHide:function(){
+    var isShow = this.data.isShow;
+    if (isShow){
+      this.setData({
+        isShow: false,
+        arrow: 'down',
+        showText: '展开'
+      });
+    }else{
+      this.setData({
+        isShow: true,
+        arrow: 'up',
+        showText: '收起'
+      });
+    }
+    
   }
 })
